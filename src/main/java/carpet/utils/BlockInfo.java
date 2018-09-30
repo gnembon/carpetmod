1b880f5da652049fbd318067c3e584b23572d85e
gnembon <gnembonmc@gmail.com>
2018-09-30 17:02:50 -0300

    wrong import

--- src/main/java/carpet/utils/BlockInfo.java
+++ src/main/java/carpet/utils/BlockInfo.java
@@ -1,6 +1,5 @@
 package carpet.utils;
 
-import com.sun.deploy.panel.IProperty;
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
