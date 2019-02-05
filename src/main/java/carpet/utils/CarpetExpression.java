e882ddea89996bb7aec38915b195a4a2bcb95d23
gnembon <gnembonmc@gmail.com>
2019-02-04 22:35:43 -0400

    simplifying carpet expressions

--- src/main/java/carpet/utils/CarpetExpression.java
+++ src/main/java/carpet/utils/CarpetExpression.java
@@ -142,285 +142,144 @@ public class CarpetExpression
         this.source = source;
         this.expr = new Expression(expression);
 
-        this.expr.addFunction(new AbstractFunction("block", -1)
+        this.expr.addFunction("block", (lv) ->
         {
-            @Override
-            public Value eval(List<Value> params)
-            {
-                if (params.size() == 0)
+                if (lv.size() == 0)
                 {
                     throw new ExpressionException("block requires at least one parameter");
                 }
-                if (params.size() == 1)
+                if (lv.size() == 1)
                 {
-                    return new BlockValue(IRegistry.field_212618_g.get(new ResourceLocation(params.get(0).getString())).getDefaultState(), origin);
+                    return new BlockValue(IRegistry.field_212618_g.get(new ResourceLocation(lv.get(0).getString())).getDefaultState(), origin);
                 }
-                BlockPos pos = locateBlockPos(params);
+                BlockPos pos = locateBlockPos(lv);
                 return new BlockValue(source.getWorld().getBlockState(pos), pos);
-            }
         });
 
-        this.expr.addFunction(new AbstractFunction("solid", -1, true)
+        this.expr.addFunction("solid", (lv) -> booleanStateTest("solid", lv, (s, p) -> s.isSolid()));
+
+        this.expr.addFunction("air", (lv) -> booleanStateTest("air", lv, (s, p) -> s.isAir()));
+
+        this.expr.addFunction("liquid", (lv) -> booleanStateTest("liquid", lv, (s, p) -> !s.getFluidState().isEmpty()));
+
+        this.expr.addNAryFunction("light", 3, (lv) ->
+                new NumericValue(source.getWorld().getLight(locateBlockPos(lv))));
+
+        this.expr.addNAryFunction("blockLight", 3, (lv) ->
+                new NumericValue(source.getWorld().getLightFor(EnumLightType.BLOCK, locateBlockPos(lv))));
+
+        this.expr.addNAryFunction("skyLight", 3, (lv) ->
+                new NumericValue(source.getWorld().getLightFor(EnumLightType.SKY, locateBlockPos(lv))));
+
+        this.expr.addNAryFunction("loaded", 3, (lv) ->
+                source.getWorld().isBlockLoaded(locateBlockPos(lv)) ? Value.TRUE : Value.FALSE);
+
+        this.expr.addNAryFunction("loadedEP", 3, (lv) ->
         {
-            @Override
-            public Value eval(List<Value> lazyParams)
-            {
-                return booleanStateTest("solid", lazyParams, (s, p) -> s.isSolid());
-            }
+            BlockPos pos = locateBlockPos(lv);
+            return source.getWorld().isAreaLoaded(pos.getX() - 32, 0, pos.getZ() - 32,
+                    pos.getX() + 32, 0, pos.getZ() + 32, true) ? Value.TRUE : Value.FALSE;
         });
 
-        this.expr.addFunction(new AbstractFunction("air", -1, true)
-        {
-            @Override
-            public Value eval(List<Value> lazyParams)
-            {
-                return booleanStateTest("air", lazyParams, (s, p) -> s.isAir());
-            }
-        });
+        this.expr.addFunction("suffocates", (lv) ->
+                booleanStateTest("suffocates", lv, (s, p) -> s.causesSuffocation()));
 
-        this.expr.addFunction(new AbstractFunction("liquid", -1, true)
-        {
-            @Override
-            public Value eval(List<Value> lazyParams)
-            {
-                return booleanStateTest("liquid", lazyParams, (s, p) -> !s.getFluidState().isEmpty());
-            }
-        });
+        this.expr.addNAryFunction("power", 3, (lv) ->
+                new NumericValue(source.getWorld().getRedstonePowerFromNeighbors(locateBlockPos(lv))));
 
-        this.expr.addFunction(new AbstractFunction("light", 3, false)
-        {
-            @Override
-            public Value eval(List<Value> params)
-            {
-                return new NumericValue(source.getWorld().getLight(locateBlockPos(params)));
-            }
-        });
+        this.expr.addFunction("ticksRandomly", (lv) ->
+                booleanStateTest("ticksRandomly", lv, (s, p) -> s.needsRandomTick()));
 
-        this.expr.addFunction(new AbstractFunction("blockLight", 3, false)
-        {
-            @Override
-            public Value eval(List<Value> params)
-            {
-                return new NumericValue(source.getWorld().getLightFor(EnumLightType.BLOCK, locateBlockPos(params)));
-            }
-        });
-
-        this.expr.addFunction(new AbstractFunction("skyLight", 3, false)
-        {
-            @Override
-            public Value eval(List<Value> params)
-            {
-                return new NumericValue(source.getWorld().getLightFor(EnumLightType.SKY, locateBlockPos(params)));
-            }
-        });
-
-        this.expr.addFunction(new AbstractFunction("loaded", 3, true)
-        {
-            @Override
-            public Value eval(List<Value> params)
-            {
-                return source.getWorld().isBlockLoaded(locateBlockPos(params)) ? Value.TRUE : Value.FALSE;
-            }
-        });
-
-        this.expr.addFunction(new AbstractFunction("loadedEP", 3, true)
-        {
-            @Override
-            public Value eval(List<Value> params)
-            {
-                BlockPos pos = locateBlockPos(params);
-                return source.getWorld().isAreaLoaded(
-                        pos.getX() - 32, 0, pos.getZ() - 32,
-                        pos.getX() + 32, 0, pos.getZ() + 32, true) ? Value.TRUE : Value.FALSE;
-            }
-        });
-
-        this.expr.addFunction(new AbstractFunction("suffocates", -1, true)
-        {
-            @Override
-            public Value eval(List<Value> params)
-            {
-                return booleanStateTest("suffocates", params, (s, p) -> s.causesSuffocation());
-            }
-        });
-
-        this.expr.addFunction(new AbstractFunction("power", 3, false)
-        {
-            @Override
-            public Value eval(List<Value> params)
-            {
-                return new NumericValue(source.getWorld().getRedstonePowerFromNeighbors(locateBlockPos(params)));
-            }
-        });
-
-        this.expr.addFunction(new AbstractFunction("ticksRandomly", -1, true)
-        {
-            @Override
-            public Value eval(List<Value> params)
-            {
-                return booleanStateTest("ticksRandomly", params, (s, p) -> s.needsRandomTick());
-            }
-        });
-
-        this.expr.addFunction(new AbstractFunction("update", -1, true)
-        {
-            @Override
-            public Value eval(List<Value> params)
-            {
-                return booleanStateTest("update", params, (s, p) ->
+        this.expr.addFunction("update", (lv) ->
+                booleanStateTest("update", lv, (s, p) ->
                 {
                     source.getWorld().neighborChanged(p, s.getBlock(), p);
                     return true;
-                });
-            }
-        });
+                }));
 
-        this.expr.addFunction(new AbstractFunction("forcetick", -1, true)
-        {
-            @Override
-            public Value eval(List<Value> params)
-            {
-                return booleanStateTest("forcetick", params, (s, p) ->
+        this.expr.addFunction("forcetick",(lv) ->
+                booleanStateTest("forcetick", lv, (s, p) ->
                 {
-
                     s.randomTick(source.getWorld(), p, source.getWorld().rand);
                     return true;
-                });
-            }
-        });
+                }));
 
-        this.expr.addFunction(new AbstractFunction("tick", -1, true)
-        {
-            @Override
-            public Value eval(List<Value> params)
-            {
-                return booleanStateTest("tick", params, (s, p) ->
+        this.expr.addFunction("tick", (lv) ->
+                booleanStateTest("tick", lv, (s, p) ->
                 {
                     if (s.needsRandomTick() || s.getFluidState().getTickRandomly())
-                    {
                         s.randomTick(source.getWorld(), p, source.getWorld().rand);
-                    }
                     return true;
-                });
-            }
-        });
+                }));
 
-        this.expr.addFunction(new AbstractFunction("set", -1, true)
+        this.expr.addFunction("set", (lv) ->
         {
-            @Override
-            public Value eval(List<Value> params)
+            if (lv.size() < 4 || lv.size() % 2 == 1)
+                throw new CarpetExpressionException("set block should have at least 4 params and odd attributes");
+            BlockPos pos = locateBlockPos(lv);
+            if (!(lv.get(3) instanceof BlockValue))
+                throw new CarpetExpressionException("fourth parameter of set should be a block");
+            IBlockState bs = ((BlockValue) lv.get(3)).blockState;
+
+            // TODO back off if block is the same and
+            StateContainer<Block, IBlockState> states = bs.getBlock().getStateContainer();
+
+            for (int i = 4; i < lv.size(); i += 2)
             {
+                String paramString = lv.get(i).getString();
+                IProperty<?> property = states.getProperty(paramString);
+                if (property == null)
+                    throw new CarpetExpressionException("property " + paramString + " doesn't apply to " + lv.get(3).getString());
 
-                if (params.size() < 4 || params.size() % 2 == 1)
-                {
-                    throw new CarpetExpressionException("set block should have at least 4 params and odd attributes");
-                }
-                BlockPos pos = locateBlockPos(params);
-                if (!(params.get(3) instanceof BlockValue))
-                {
-                    throw new CarpetExpressionException("fourth parameter of set should be a block");
-                }
-                IBlockState bs = ((BlockValue) params.get(3)).blockState;
+                String paramValue = lv.get(i + 1).getString();
 
-                // TODO back off if block is the same and
+                // TODO make sure properties set result in different from the location it sets too
 
-                StateContainer<Block, IBlockState> states = bs.getBlock().getStateContainer();
-
-                for (int i = 4; i < params.size(); i += 2)
-                {
-                    String paramString = params.get(i).getString();
-                    IProperty<?> property = states.getProperty(paramString);
-                    if (property == null)
-                    {
-                        throw new CarpetExpressionException("property " + paramString + " doesn't apply to " + params.get(3).getString());
-                    }
-
-                    String paramValue = params.get(i + 1).getString();
-
-                    // TODO make sure properties set result in different from the location it sets too
-
-                    bs = setProperty(property, paramString, paramValue, bs);
-                }
-                source.getWorld().setBlockState(pos, bs, 2 | (CarpetSettings.getBool("fillUpdates") ? 0 : 1024));
-                return new BlockValue(bs, pos);
+                bs = setProperty(property, paramString, paramValue, bs);
             }
+            source.getWorld().setBlockState(pos, bs, 2 | (CarpetSettings.getBool("fillUpdates") ? 0 : 1024));
+            return new BlockValue(bs, pos);
         });
 
-        this.expr.addFunction(new AbstractFunction("blocksMovement", -1, true)
+        this.expr.addFunction("blocksMovement", (lv) ->
+                booleanStateTest("blocksMovement", lv, (s, p) ->
+                        !s.allowsMovement(source.getWorld(), p, PathType.LAND)));
+
+        this.expr.addFunction("sound", (lv) ->
+                stateStringQuery("sound", lv, (s, p) ->
+                        BlockInfo.soundName.get(s.getBlock().getSoundType())));
+
+        this.expr.addFunction("material",(lv) ->
+                stateStringQuery("material", lv, (s, p) ->
+                        BlockInfo.materialName.get(s.getMaterial())));
+
+        this.expr.addFunction("mapColour", (lv) ->
+                stateStringQuery("mapColour", lv, (s, p) ->
+                        BlockInfo.mapColourName.get(s.getMapColor(source.getWorld(), p))));
+
+        this.expr.addBinaryFunction("property", (v1, v2) ->
         {
-            @Override
-            public Value eval(List<Value> params)
-            {
-                return booleanStateTest("blocksMovement", params, (s, p)
-                        -> !s.allowsMovement(source.getWorld(), p, PathType.LAND));
-            }
-        });
-
-        this.expr.addFunction(new AbstractFunction("sound", -1, true)
-        {
-            @Override
-            public Value eval(List<Value> params)
-            {
-                return stateStringQuery("sound", params, (s, p)
-                        -> BlockInfo.soundName.get(s.getBlock().getSoundType()));
-            }
-        });
-
-        this.expr.addFunction(new AbstractFunction("material", -1, true)
-        {
-            @Override
-            public Value eval(List<Value> params)
-            {
-                return stateStringQuery("material", params, (s, p)
-                        -> BlockInfo.materialName.get(s.getMaterial()));
-            }
-        });
-
-        this.expr.addFunction(new AbstractFunction("mapColour", -1, true)
-        {
-            @Override
-            public Value eval(List<Value> params)
-            {
-                return stateStringQuery("mapColour", params, (s, p)
-                        -> BlockInfo.mapColourName.get(s.getMapColor(source.getWorld(), p)));
-            }
-        });
-
-        this.expr.addFunction(new AbstractFunction("property", 2, false)
-        {  // why not
-            @Override
-            public Value eval(List<Value> params)
-            {
-                if (!(params.get(0) instanceof BlockValue))
+                if (!(v1 instanceof BlockValue))
                     throw new ExpressionException("First Argument of tag should be a block");
-                IBlockState state = ((BlockValue) params.get(0)).blockState;
-                String tag = params.get(1).getString();
+                IBlockState state = ((BlockValue) v1).blockState;
+                String tag = v2.getString();
                 StateContainer<Block, IBlockState> states = state.getBlock().getStateContainer();
                 IProperty<?> property = states.getProperty(tag);
                 if (property == null)
                     return Value.EMPTY;
                 return new StringValue(state.get(property).toString());
-            }
+
         });
 
-        this.expr.addFunction(new AbstractFunction("print", 1)
-        { // TODO make sure it masks vanilla implementation
-            @Override
-            public Value eval(List<Value> params)
-            {
-                Value arg = params.get(0);
-                Messenger.m(source, "gi " + arg.getString());
-                return arg; // pass through for variables
-            }
-        });
-
-        this.expr.addFunction(new AbstractFunction("conv", 1)
+        this.expr.addUnaryFunction("print", (v) ->
         {
-            @Override
-            public Value eval(List<Value> params)
-            {
-                throw new UnsupportedOperationException(); // TODO
-            }
+            Messenger.m(source, "gi " + v.getString());
+            return v; // pass through for variables
+        });
+
+        this.expr.addUnaryFunction("conv", (v)->
+        {
+            throw new UnsupportedOperationException(); // TODO
         });
 
 
