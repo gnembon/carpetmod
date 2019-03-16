44a853109e14e84ef9adf7ce4419f4f663635cbd
DeadlyMC <modding.deadlymc@gmail.com>
2019-03-16 22:01:00 +0530

    added renewable packed ice

--- src/main/java/carpet/CarpetSettings.java
+++ src/main/java/carpet/CarpetSettings.java
@@ -190,7 +190,7 @@ public class CarpetSettings
   /////rule("itemFrameDuplicationFix", "fix", "Fixes duplication of items when using item frames"),
   /////rule("craftingWindowDuplicationFix", "fix", "Fixes the recipe book duplication caused by clicking to fast while crafting"),
   rule("silverFishDropGravel",  "experimental", "Silverfish drop a gravel item when breaking out of a block"),
-  /////rule("renewablePackedIce",    "experimental", "Multiple ice crushed by falling anvils make packed ice"),
+  rule("renewablePackedIce",    "experimental", "Multiple ice crushed by falling anvils make packed ice"),
   /////rule("renewableDragonEggs",   "experimental", "Dragon eggs when fed meet items spawn more eggs"),
   //!rule("summonNaturalLightning","creative", "summoning a lightning bolt has all the side effects of natural lightning"),
   rule("commandSpawn",          "commands", "Enables /spawn command for spawn tracking").isACommand(),
