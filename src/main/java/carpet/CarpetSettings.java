04d52328f04bc77b234cc28863359c0a32d8bebd
DeadlyMC <modding.deadlymc@gmail.com>
2019-03-16 22:22:18 +0530

    added renewable dragon eggs

--- src/main/java/carpet/CarpetSettings.java
+++ src/main/java/carpet/CarpetSettings.java
@@ -191,7 +191,7 @@ public class CarpetSettings
   /////rule("craftingWindowDuplicationFix", "fix", "Fixes the recipe book duplication caused by clicking to fast while crafting"),
   rule("silverFishDropGravel",  "experimental", "Silverfish drop a gravel item when breaking out of a block"),
   /////rule("renewablePackedIce",    "experimental", "Multiple ice crushed by falling anvils make packed ice"),
-  /////rule("renewableDragonEggs",   "experimental", "Dragon eggs when fed meet items spawn more eggs"),
+  rule("renewableDragonEggs",   "experimental", "Dragon eggs when fed meet items spawn more eggs"),
   //!rule("summonNaturalLightning","creative", "summoning a lightning bolt has all the side effects of natural lightning"),
   rule("commandSpawn",          "commands", "Enables /spawn command for spawn tracking").isACommand(),
   rule("commandTick",           "commands", "Enables /tick command to control game speed").isACommand(),
