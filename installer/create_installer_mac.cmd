d805401c96879e65b5c375e58ed431b3d72b90c1
gnembon <gnembonmc@gmail.com>
2019-04-20 11:37:38 -0300

    verbump to easter edition

--- installer/create_installer_mac.cmd
+++ installer/create_installer_mac.cmd
@@ -14,8 +14,8 @@ mkdir output-mac
 echo Copying files ...
 copy ..\build\distributions\carpetmod_1.13.2_Client.zip output-mac > nul
 copy ..\build\distributions\carpetmod_1.13.2_Server.zip output-mac > nul
-copy mac_install_server.cmd output-mac > nul
-copy mac_install_singleplayer.cmd output-mac > nul
+copy mac_install_server.sh output-mac > nul
+copy mac_install_singleplayer.sh output-mac > nul
 copy README.txt output-mac > nul
 copy 1.13.2-carpet.json output-mac > nul
 if exist carpet_package_mac.zip del /q carpet_package_mac.zip
